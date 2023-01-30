8fdd7a4b83ee7e8bd6fcfa7aa3492d42a39f418d
dewanwakilahmed <dewanwakilahmed@gmail.com>
2023-01-30 21:17:38 +0600

    Writing the First Endpoint

--- src/main/java/com/dewanwakilahmed/movies/MoviesApplication.java
+++ src/main/java/com/dewanwakilahmed/movies/MoviesApplication.java
@@ -2,12 +2,21 @@ package com.dewanwakilahmed.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MoviesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+	@GetMapping("/root")
+	public String apiRoot() {
+		return "Hello World!";
 	}
 
 }
